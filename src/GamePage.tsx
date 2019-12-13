f3f225fb58c6a07341a89e333b01683fe44ee82c
kcygan <kacper.cygan@deviniti.com>
2019-12-13 14:13:33 +0100

    Modified game page

--- src/GamePage.tsx
+++ src/GamePage.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
-import { FC } from 'react';
-import { Text } from 'grommet';
+import { FC, useState } from 'react';
+import { Text, Box, List } from 'grommet';
+
+type Player = {
+    name: string;
+    isReady?: boolean;
+}
+
+const somePlayers: Player[] = [
+    {
+        name: 'Tomek',
+        isReady: true
+    },
+    {
+        name: 'Żuraw',
+        isReady: true
+    },
+    {
+        name: 'Michał',
+        isReady: true
+    },
+    {
+        name: 'Kacper',
+        isReady: true
+    }
+]
 
 const GamePage: FC = () => {
-    return (<><Text>Hello world</Text></>)
+    const [currentPlayers, setCurrentPlayers] = useState(somePlayers);
+
+
+    return (<Box justify="center" align="center" pad="small" gap="medium">
+        <Text size="xlarge">Obecna rozgrywka:</Text>
+        <Box direction="column">
+            <Box direction="row">
+                <Box></Box>
+                <Box></Box>
+            </Box>
+            <Box direction="row">
+                <Box></Box>
+                <Box></Box>
+            </Box>
+        </Box>
+    </Box>)
 };
+
+const NoPlayersMessage: FC = () => {
+    return (<>
+        <Text size="large" alignSelf="center" margin={{ top: '125px' }}>
+            Brak chętnych
+        </Text></>
+    )
+}
+
+const Player: FC<{ player: Player }> = ({ player }) => {
+
+    return (<Box width={'100%'}>
+    </Box>)
+}
 
 export default GamePage;
