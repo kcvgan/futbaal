d8b6a6a22d7afaff244b85e0ed78479d3f2e521f
kcygan <kacper.cygan@deviniti.com>
2019-12-13 13:39:46 +0100

    Added routing and gamepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { FC, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import { Grommet, Box } from 'grommet';
+import GamePage from './GamePage';
 
 const theme = {
   global: {
@@ -12,20 +15,32 @@ const theme = {
   },
 };
 
-const App: React.FC = () => {
+const App: FC = () => {
+  const [isAuth, setIsAuth] = useState(true);
+
   return (
     <Grommet theme={theme}>
-       <Box
-      direction="row-responsive"
-      justify="center"
-      align="center"
-      pad="medium"
-      gap="medium"
-    >
-      <LoginPage/>
+      <Box
+        direction="row-responsive"
+        justify="center"
+        align="center"
+        pad="medium"
+        gap="medium"
+      >
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <LoginPage />
+            </Route>
+            {isAuth && <Route path="/game">
+              <GamePage />
+            </Route>}
+          </Switch>
+        </Router>
       </Box>
     </Grommet>
   );
+
 }
 
 export default App;
