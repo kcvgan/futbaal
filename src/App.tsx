604737b8e5a1a368cbfdd0ea8907c49fabba8c2a
kcygan <kacper.cygan@deviniti.com>
2019-12-13 13:20:50 +0100

    Added basic login page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LoginPage from './LoginPage';
+import { Grommet, Box } from 'grommet';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme}>
+       <Box
+      direction="row-responsive"
+      justify="center"
+      align="center"
+      pad="medium"
+      gap="medium"
+    >
+      <LoginPage/>
+      </Box>
+    </Grommet>
   );
 }
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
 
 export default App;
